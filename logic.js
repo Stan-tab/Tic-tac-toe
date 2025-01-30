20807a84e3555e0d1fcc53114dc23a3d615f77a1
Stan-tab <godofwarmafia2@gmail.com>
2025-01-30 21:00:50 +0500

    feat:Add winLogic for xy accis

--- logic.js
+++ logic.js
@@ -16,7 +16,7 @@ const gameStart = (() => {
     gamers.push(player);
   }
 
-  const adder = (pos = []) => {
+  const adder = (pos) => {
     if(pos.length != 2) {console.error("Invalid size of array"); return 1;};
     if(pos[0]>2 || pos[0]<0 || pos[1]>2 || pos[1]<0) {console.error("Invalid number of value"); return 1;};
     if(arrayList[pos[0]][pos[1]] != 0){console.error("Can not add properties it is full"); return 1;};
@@ -27,6 +27,18 @@ const gameStart = (() => {
       arrayList[pos[0]][pos[1]] = gamers[1].sign;
       queue[0] = 0;
     }
+    winCheck(pos, Number(!queue[0]))
+  }
+
+  function winCheck(pos, winner) {
+    let value = arrayList[pos[0]][pos[1]];
+    let x = 0;
+    let y = 0;
+    for (let i = 0; i<3; i++){
+      if(value == arrayList[i][pos[1]]) x++;
+      if(value == arrayList[pos[0]][i]) y++;
+    }
+    if(x==3 || y == 3) {console.log(`${gamers[winner].name} win`);  return 0}
   }
 
   function restart () {
