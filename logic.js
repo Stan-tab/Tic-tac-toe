5734eee05b6989ecc0d7c815993c5fca5e5003b4
Stan-tab <godofwarmafia2@gmail.com>
2025-01-31 15:02:26 +0500

    feat: change win alert

--- logic.js
+++ logic.js
@@ -63,7 +63,7 @@ const gameStart = (() => {
       if(value == arrayList[i][i]) xy1++;
       if(value == arrayList[i][2-i]) xy2++;
     }
-    if(x==3 || y == 3 || xy1 == 3 || xy2 == 3) {alert(`${gamers[winner].name} win`); win[0] = 1; return true}
+    if(x==3 || y == 3 || xy1 == 3 || xy2 == 3) {alert(`${gamers[winner].name} win! \nNow ${gamers[1].name} turn`); win[0] = 1; return true}
     arrayList.forEach(el => {
       el.forEach(e => {
         if (e != 0) ar++;
